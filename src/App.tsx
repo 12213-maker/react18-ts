f9c9c770ca853a774e736f27871a372f1791399c
李楠林 <linanlin@bilibili.com>
2023-04-13 21:01:46 +0800

    06.路由懒加载+Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React, { memo } from 'react'
+import React, { memo, Suspense } from 'react'
 import { useRoutes, Link } from 'react-router-dom'
 import routes from './router'
 import Focus from './views/focus'
 export default memo(function App() {
   return (
-    <div>
+    <div className='App'>
       {/* 在组件里面传东西，会传入到props.children里面去，但是需要你自己定义children属性 */}
-      <Link to='/discover' state={{ name: 'lnl' }}>discover</Link>
-      <Link to='/mine' state={{ name: 'lnl' }}>mine</Link>
-      <Link to='/focus' state={{ name: 'lnl' }}>focus</Link>
-      <Link to='/download' state={{ name: 'lnl' }}>download</Link>
-      {useRoutes(routes)}
+
+
+      <div className='nav'>
+        <Link to='/discover' >发现音乐</Link>
+        <Link to='/mine' >我的音乐</Link>
+        <Link to='/focus' >关注</Link>
+        <Link to='/download' >下载客户端</Link>
+      </div>
+
+      {/* 因为路由会进行懒加载，所以使用suspense对其进行包裹 */}
+      {/* <div className='main'>{useRoutes(routes)}</div> */}
+
+      <Suspense fallback={'loadi11111111111111111111111111111ng...'}>
+        <div className='main'>{useRoutes(routes)}</div>
+      </Suspense>
+
+
     </div>
   )
 })
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,21 @@
-import Discover from '@/views/discover'
-import Download from '@/views/download'
-import Focus from '@/views/focus'
-import Mine from '@/views/mine'
+import { lazy } from 'react'
 import { Navigate, RouteObject } from 'react-router-dom'
+// import Discover from '@/views/discover'
+// import Download from '@/views/download'
+// import Focus from '@/views/focus'
+// import Mine from '@/views/mine'
+
+//使用lazy对组件进行懒加载,但是点击的时候可能还没有下载下来，所以要搭配suspense使用
+const Discover = lazy(() => import('@/views/discover'));
+const Download = lazy(() => import('@/views/download'));
+const Focus = lazy(() => import('@/views/focus'));
+const Mine = lazy(() => import('@/views/mine'));
 
 //使用RouteObject
 const routes: RouteObject[] = [
   {
-    path:'/',
-    element:<Navigate to='/discover'></Navigate>
+    path: '/',
+    element: <Navigate to='/discover'></Navigate>
   },
   {
     path: '/discover',
