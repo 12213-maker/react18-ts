d28466428c5ae1780f0c49b2c799e0679a1d2352
李楠林 <linanlin@bilibili.com>
2023-04-13 16:38:57 +0800

    04.craco-less配置less的使用

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const CracoLess = require('craco-less')
 const getpath = (dir) => path.resolve(__dirname, dir)
 module.exports = {
+  plugins: [{ plugin: CracoLess }],
   webpack: {
     //配置别名
     alias: {
